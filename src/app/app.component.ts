aedad69035f1c82961f2c0aa5f0a541738e63022
Szymon Wiergowski <s.wiergowski@gmail.com>
2020-05-31 21:46:00 +0200

    add ngForto project

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { Task } from './task';
 
 @Component({
   selector: 'app-root',
@@ -6,13 +7,32 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css'],
 })
 export class AppComponent {
-  title = 'Lista Zadań';
+  config: { [key: string]: string | Date } = null;
+  tasks: Task[] = [
+    {
+      name: 'Siłownia',
+      deadline: '2020-01-02',
+      done: false,
+    },
+    {
+      name: 'Nauka Angulara',
+      deadline: '2020-01-03',
+      done: false,
+    },
+    {
+      name: 'Sprzątanie kuwety',
+      deadline: '2020-01-04',
+      done: false,
+    },
+  ];
 
-  get footer(): string {
-    return '2020 © Lista zadań,All rights reserved.';
-  }
-
-  getDate(): Date {
-    return new Date();
+  constructor() {
+    setTimeout(() => {
+      this.config = {
+        title: 'Lista zadań',
+        footer: '© Lista zadań zbudowana w Angularze.',
+        date: new Date(),
+      };
+    }, 500);
   }
 }
